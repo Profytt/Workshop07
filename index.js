4ee6e142b4362a6599de5bfd96487d8fb76a2de7
Profytt <dezcoleman2@gmail.com>
2024-01-30 22:54:53 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//create 3 variables
+let firstNumber;
+
+let secondNumber;
+
+let thirdNumber;
+
+// create 3 equations to equal 10, 40, 39
+firstNumber = 5 + 5;
+
+secondNumber = 20 * 2;
+
+thirdNumber = 42 - 3
+
+
+const secreMessage = `
+You have received this message because you have been chosen to open an important vault.
+Here is the secret combination: ${firstNumber}, ${secondNumber}, ${thirdNumber}
+
+`;
+
+
+
+alert(secreMessage);
---
+++ vaultcodes.js
@@ -0,0 +1,13 @@
+// create string " You have received this message because you have been chosen to open an important vault. Here is the secret combination: " //
+const str = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+// assign three constiables : 10 40 39 //
+const num1 = code1
+const num2 = code2
+const num3 = code3
+// assign coresponding constiables calculations //
+const code1 = 5 + 5;
+const code2 = 20 * 2;
+const code3 = 42 - 3;
+
+// create alert displaying message and codes /
+alert(str + " " + code1  + code2  + code3);
